2e25fc2a56665a4ea10f62c36e4ee1f061e4604d
kdauler <kolby.dauler@gmail.com>
2019-09-30 18:17:55 -0700

    update the updater

--- provisioning/update.sh
+++ provisioning/update.sh
@@ -1,1 +1,5 @@
-aws elasticbeanstalk update-environment --application-name invoicer --environment-id e-saebueqdau --version-label latest-invoicer  --region us-west-1
+#/bin/bash
+
+ID=$1
+
+aws elasticbeanstalk update-environment --application-name invoicer --environment-id $ID --version-label latest-invoicer  --region us-west-1
